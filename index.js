c883f6ec7326878a945851146d1d0fdb2e0f614f
Amir Alipour <amir.alipour12127@gmail.com>
2021-07-08 20:31:15 +0430

    main disabler added to index js

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 //helpers
-
 function isFunction(obj) {
     return typeof obj == 'function';
 }
@@ -7,4 +6,27 @@ function isFunction(obj) {
 function isObject(obj) {
     var type = typeof obj;
     return type === 'function' || (type === 'object' && !!obj);
+}
+
+
+// main functionality
+export function disableReactDevTools() {
+    // Ensure the React Developer Tools global hook exists
+    if (!isObject(window.__REACT_DEVTOOLS_GLOBAL_HOOK__)) {
+      return;
+    }
+  
+    // Replace all global hook properties with a no-op function or a null value
+    for (const prop in window.__REACT_DEVTOOLS_GLOBAL_HOOK__) {
+      if (prop === "renderers") {
+        // prevents console error when dev tools try to iterate of renderers
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = new Map();
+        continue;
+      }
+      window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop] = isFunction(
+        window.__REACT_DEVTOOLS_GLOBAL_HOOK__[prop]
+      )
+        ? Function.prototype
+        : null;
+    }
 }
